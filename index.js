e4e573bfe4bb3aa17438b392d35026eee0ebbc0c
Paul-Cl-ark <clark.paul123@gmail.com>
2018-12-20 16:57:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function Scooter(year, colour, model) {
+  this.year = year
+  this.colour = colour
+  this.model = model
+}
+function Driver(name, age, haircolour) {
+  this.name = name
+  this.age = age
+  this.haircolour = haircolour
+}
+function PickupLocation(bing, bang) {
+  this.bing = bing
+  this.bang = bang
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -6,7 +6,7 @@ describe('objects', function() {
       let samson = new Scooter(2015, "red", "swoosh");
       expect(samson).to.be.an.instanceof(Scooter);
       expect(samson.year).to.equal(2015);
-      expect(samson.color).to.equal("red");
+      expect(samson.colour).to.equal("red");
       expect(samson.model).to.equal("swoosh");
     });
 
@@ -18,7 +18,7 @@ describe('objects', function() {
       expect(allison).to.be.an.instanceof(Driver);
       expect(allison.name).to.equal("Allison");
       expect(allison.age).to.equal("16 years");
-      expect(allison.experience).to.equal("2 years");
+      expect(allison.haircolour).to.equal("2 years");
     });
   });
 
@@ -26,8 +26,8 @@ describe('objects', function() {
     it("should create a new PickupLocation with two properties", function(){
       let poshHotel = new PickupLocation("123 Broadway", "New York City");
       expect(poshHotel).to.be.an.instanceof(PickupLocation);
-      expect(poshHotel.address).to.equal("123 Broadway");
-      expect(poshHotel.city).to.equal("New York City");
+      expect(poshHotel.bing).to.equal("123 Broadway");
+      expect(poshHotel.bang).to.equal("New York City");
     });
   });
 });
